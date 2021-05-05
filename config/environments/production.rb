6333c87e994c1c64d35348e18fe9bc00155dee1c
Sophie Bonneval <maldorana@gmail.com>
2021-05-05 11:47:58 +0100

    Redis install & setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,4 +117,6 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  config.web_socket_server_url = "wss://message-me-on-rails.herokuapp.com//cable"
+  config.action_cable.allowed_request_origins = ['https://message-me-on-rails.herokuapp.com/', 'https://message-me-on-rails.herokuapp.com/']
 end
