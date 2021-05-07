cb40fc6fac375ebaef0b2f9ed46b5dec4e656d80
Sophie Bonneval <maldorana@gmail.com>
2021-05-07 11:03:33 +0100

    Add user menu backend

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -3,6 +3,7 @@ class ChatroomController < ApplicationController
     def index
         @message = Message.new
         @messages = Message.last_messages
+        @users = User.all
     end
 
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,5 @@
 class Message < ApplicationRecord
     belongs_to :user
     validates :body, presence: true, length: { maximum: 300 }
-    scope :last_messages, -> { order(:created_at).last(20) }
+    scope :last_messages, -> { order(:created_at).last(50) }
 end
